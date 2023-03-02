86df52d37eda453388606b8af28406118aa70cbb
AADI <aaditya9458@gmail.com>
2023-03-02 14:12:47 +0530

    installation2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/300.css'
+import { Button } from '@mui/material'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <Button variant='text'>Text</Button>
+      <Button variant='contained' color="secondary" size='small'>Contained</Button>
+      <Button variant='outlined'>Outlined</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
